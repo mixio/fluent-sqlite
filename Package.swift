acdbbb45ec81294f0ace455ad932499f89546190
Jean Jourdain <jean.jourdain@mixio.com>
2018-08-31 12:54:25 +0200

    First shot at implementing multiple joins to the same table in vapor/sql.
    1. Added support for table aliases in vapor/sql
    2. Added support for extracting occurrences of multiple occurrences of a Model in a resultset.
    
    See alias: and occurence: in code:
    
        let result = try conn.select()
            .all()
            .from(Message.self)
            .join(\Message.from_person_id, to:\Person.id, alias: .identifier("F"))
            .join(\Message.to_person_id, to:\Person.id, alias: .identifier("T"))
            .where(\Message.id == message.requireID())
            .all()
            .map { [conn] rows in
                try rows.map { [conn] row -> (Message, Person, Person) in
                    // Using table aliases.
                    let msg = try conn.decode(Message.self, from: row, table: "messages")
                    let from = try conn.decode(Person.self, from: row, table: "persons", occurrence: 1)
                    let to = try conn.decode(Person.self, from: row, table: "persons", occurrence: 2)
                    return (msg, from, to)
                }
            }
            .wait()

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
-        
+
         // ✳️ Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
+        .package(url: "https://github.com/mixio/fluent.git", from: "table-aliases-and-resultset-occurrences"),
 
         // 📦 Dependency injection / inversion of control framework.
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
 
         // 🔵 SQLite 3 wrapper for Swift
-        .package(url: "https://github.com/vapor/sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/mixio/sqlite.git", from: "table-aliases-and-resultset-occurrences"),
     ],
     targets: [
         .target(name: "FluentSQLite", dependencies: ["Async", "FluentSQL", "Service", "SQLite"]),
