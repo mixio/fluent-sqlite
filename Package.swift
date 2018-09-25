3c09c0a1b97b7711ef65f925a455cdfdc48cd65c
Jean Jourdain <jean.jourdain@mixio.com>
2018-09-25 17:48:43 +0200

    New branch mixio-dev-v0.0.1

--- Package.swift
+++ Package.swift
@@ -9,18 +9,18 @@ let package = Package(
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
-//      .package(url: "https://github.com/mixio/core.git", .branch("mixio-dev")),
+//      .package(url: "https://github.com/mixio/core.git", .branch("mixio-dev-v0.0.1")),
 
         // ✳️ Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
 //      .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
-        .package(url: "https://github.com/mixio/fluent.git", .branch("mixio-dev")),
+        .package(url: "https://github.com/mixio/fluent.git", .branch("mixio-dev-v0.0.1")),
 
         // 📦 Dependency injection / inversion of control framework.
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
 
         // 🔵 SQLite 3 wrapper for Swift
 //      .package(url: "https://github.com/vapor/sqlite.git", from: "3.0.0"),
-        .package(url: "https://github.com/mixio/sqlite.git", .branch("mixio-dev")),
+        .package(url: "https://github.com/mixio/sqlite.git", .branch("mixio-dev-v0.0.1")),
     ],
     targets: [
         .target(name: "FluentSQLite", dependencies: ["Async", "FluentSQL", "Service", "SQLite"]),
